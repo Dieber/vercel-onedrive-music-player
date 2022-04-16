76d2dd59734b981f525a92d5fad430df5930398d
Dieber <a451120068@gmail.com>
2022-04-16 17:41:48 +0800

    solid

--- pages/setup/step2.tsx
+++ pages/setup/step2.tsx
@@ -7,59 +7,12 @@ import { generateAuthorisationUrl } from "../../utils/token";
 import { useState } from "react";
 import { post } from "../../utils/fetcher";
 
-const authUrl = generateAuthorisationUrl();
-
 export default function Step2() {
   const router = useRouter();
 
-  const [oAuthCode, setOAuthCode] = useState("");
-
-  // const { t } = useTranslation();
-
   return (
     <AppLayout>
-      <StepLayout from={1} to={2}>
-        <div>
-          <div className="w-full">
-            <button
-              onClick={() => {
-                window.open(authUrl);
-              }}
-            >
-              OAuth OneDrive Account!
-            </button>
-          </div>
-          <div>
-            <input
-              className="text-black"
-              value={oAuthCode}
-              onChange={(e) => {
-                setOAuthCode(e.target.value);
-              }}
-            ></input>
-            <button
-              disabled={!oAuthCode}
-              onClick={() => {
-                if (!oAuthCode) {
-                  return;
-                }
-
-                post(`/api/storeTokenByOAuth`, {
-                  code: oAuthCode,
-                })
-                  .then((e) => {
-                    console.log(e);
-                  })
-                  .catch((e) => {
-                    console.log("catch", e);
-                  });
-              }}
-            >
-              Store OAuth!
-            </button>
-          </div>
-        </div>
-      </StepLayout>
+      <StepLayout from={1} to={2}></StepLayout>
     </AppLayout>
   );
 }
